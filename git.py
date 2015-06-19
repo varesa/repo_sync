32ff95b23cc974ee5a61460b7da24983aa1a3dbb
Esa Varemo <esa@kuivanto.fi>
2015-06-19 19:15:42 +0000

    Fix an error when trying to pull after a clone

--- git.py
+++ git.py
@@ -18,15 +18,11 @@ class Git:
         out = subprocess.check_output(['git', 'clone', repo['ssh_url']], cwd=self.PATH_BASE, stderr=subprocess.STDOUT)
         out += subprocess.check_output(['git', 'remote', 'add', 'gitlab', mirror['ssh_url_to_repo']], cwd=self.repo_path(repo['name']), stderr=subprocess.STDOUT)
 
-        return out
+        return out.decode()
 
     def repo_sync(self, repo):
-        out = subprocess.check_output(['git', 'pull', 'origin', '*:*'], cwd=self.repo_path(repo['name']), stderr=subprocess.STDOUT)
-        try:
-            out += subprocess.check_output(['git', 'pull', 'gitlab', '*:*'], cwd=self.repo_path(repo['name']), stderr=subprocess.STDOUT)
-        except:
-            pass # Fails on the first run when the mirror is empty
+        out = subprocess.check_output(['git', 'pull', '--all'], cwd=self.repo_path(repo['name']), stderr=subprocess.STDOUT)
         out += subprocess.check_output(['git', 'push', 'origin', '*:*'], cwd=self.repo_path(repo['name']), stderr=subprocess.STDOUT)
         out += subprocess.check_output(['git', 'push', 'gitlab', '*:*'], cwd=self.repo_path(repo['name']), stderr=subprocess.STDOUT)
 
-        return out
+        return out.decode()
