4ad0a1b820394a36853d67eba507f7acbb13aed6
Esa Varemo <esa@kuivanto.fi>
2015-06-19 20:22:11 +0000

    Prevent crash on a failure to sync

--- git.py
+++ git.py
@@ -1,10 +1,10 @@
 import os
 import subprocess
+import traceback
 
 class Git:
     def __init__(self):
         self.PATH_BASE = os.path.join(os.path.expanduser("~"), "repo_sync")
-        print(self.PATH_BASE)
         if not os.path.isdir(self.PATH_BASE):
             os.mkdir(self.PATH_BASE)
 
@@ -15,14 +15,20 @@ class Git:
         return os.path.join(self.PATH_BASE, name)
 
     def repo_create(self, repo, mirror):
-        out = subprocess.check_output(['git', 'clone', repo['ssh_url']], cwd=self.PATH_BASE, stderr=subprocess.STDOUT)
-        out += subprocess.check_output(['git', 'remote', 'add', 'gitlab', mirror['ssh_url_to_repo']], cwd=self.repo_path(repo['name']), stderr=subprocess.STDOUT)
+        try:
+            out = subprocess.check_output(['git', 'clone', repo['ssh_url']], cwd=self.PATH_BASE, stderr=subprocess.STDOUT)
+            out += subprocess.check_output(['git', 'remote', 'add', 'gitlab', mirror['ssh_url_to_repo']], cwd=self.repo_path(repo['name']), stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError:
+            traceback.print_exc()
 
         return out.decode()
 
     def repo_sync(self, repo):
-        out = subprocess.check_output(['git', 'pull', '--all'], cwd=self.repo_path(repo['name']), stderr=subprocess.STDOUT)
-        out += subprocess.check_output(['git', 'push', 'origin', '*:*'], cwd=self.repo_path(repo['name']), stderr=subprocess.STDOUT)
-        out += subprocess.check_output(['git', 'push', 'gitlab', '*:*'], cwd=self.repo_path(repo['name']), stderr=subprocess.STDOUT)
+        try:
+            out = subprocess.check_output(['git', 'pull', '--all'], cwd=self.repo_path(repo['name']), stderr=subprocess.STDOUT)
+            out += subprocess.check_output(['git', 'push', 'origin', '*:*'], cwd=self.repo_path(repo['name']), stderr=subprocess.STDOUT)
+            out += subprocess.check_output(['git', 'push', 'gitlab', '*:*'], cwd=self.repo_path(repo['name']), stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError:
+            traceback.print_exc(),
 
         return out.decode()
